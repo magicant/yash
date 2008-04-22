0d1a2a257c543f497f54441a5192fe68c760ae47
ywtnb <ywtnb@048f04df-13f5-43d7-8114-9f9ceecaec24>
2008-04-22 05:14:29 +0000

    yash2: tests/run-all.sh: 改良 yash2: tests: スクリプト追加 	+expand, expand.posix, parser.posix, redir.posix, variable.posix
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/branches/yash2@444 048f04df-13f5-43d7-8114-9f9ceecaec24

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
 
-TESTEE=${1:-../yash}
-export LC_ALL=C LANG=C TESTEE
+if [ x"$1" = x"-p" ]; then
+    scripts='*.posix.test'
+    shift
+else
+    scripts='*.test'
+fi
 
-echo "Testing ${TESTEE}"
-echo "Any output from any test indicates a possible malfunction"
+TESTEE=${1:-../yash}
+LC_ALL=C
+IFS=' 	
+'
+export TESTEE LC_ALL IFS
+
+echo "Starting test of ${TESTEE}"
+echo "Any output from the tests indicates a possible malfunction"
 
 failed=0
-for x in *.tests
+for x in $scripts
 do
-    echo $x...
-    if ! ${TESTEE} $x 2>&1 | diff - ${x%.tests}.right
+    echo ${x%.test}...
+    if ! ${TESTEE} $x 2>&1 | diff - ${x%.test}.right
     then
-	failed=$(( ${failed} + 1 ))
+	failed=$(( failed + 1 ))
     fi
 done
 
-echo "${failed} test(s) failed."
-test 0 -eq ${failed}
+if [ 0 -eq ${failed} ]
+then
+    echo "All tests successful."
+else
+    echo "${failed} test(s) failed."
+    false
+fi
 
 
 # vim: set ts=8 sts=4 sw=4 noet:
