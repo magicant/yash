a3fc50efb5160d9252fad25111eb61b43e3e4d6b
ywtnb <ywtnb@048f04df-13f5-43d7-8114-9f9ceecaec24>
2008-04-22 15:00:14 +0000

    yash2: tests: run-all.sh -> run-test.sh yash2: tests: +invoke.c, Makefile yash2: tests: redir.posix, expand.posix, expand: テスト内容を修正
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/branches/yash2@451 048f04df-13f5-43d7-8114-9f9ceecaec24

---
+++ tests/run-test.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# run-test.sh: runs tests specified by $TESTEE and $TEST_ITEMS
+# © 2007-2008 magicant
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+echo "Testing ${TESTEE:=../yash} for ${TEST_ITEMS:?TEST_ITEMS not specified}"
+echo "Any output from the tests indicates a possible malfunction"
+
+LC_ALL=C
+export TESTEE LC_ALL
+unset IFS argv0 failed
+
+failed=0
+for x in $TEST_ITEMS
+do
+    x="${x%.test}"
+    echo " * $x"
+    if [ x"$x" = x"${x%.posix}" ]
+    then argv0="$TESTEE"
+    else argv0=sh
+    fi
+    if ! ./invoke "$TESTEE" "$argv0" "${x}.test" 2>&1 | diff - "${x}.right"
+    then
+	failed=$(( failed + 1 ))
+    fi
+done
+
+if [ 0 -eq ${failed} ]
+then
+    echo "All tests successful."
+else
+    echo "${failed} test(s) failed."
+    false
+fi
+
+
+# vim: set ts=8 sts=4 sw=4 noet:
