324c5002278280b22f531e765db96c0b4bd96a36
magicant <magicant@048f04df-13f5-43d7-8114-9f9ceecaec24>
2009-01-16 14:37:18 +0000

    yash: 著作権表記の年を 2009 に
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/branches/lineedit@872 048f04df-13f5-43d7-8114-9f9ceecaec24

--- lineedit/terminfo.h
+++ lineedit/terminfo.h
@@ -1,6 +1,6 @@
 /* Yash: yet another shell */
 /* terminfo.h: interface to terminfo and termios */
-/* (C) 2007-2008 magicant */
+/* (C) 2007-2009 magicant */
 
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
