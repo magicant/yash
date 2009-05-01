af0caf003eb7ceca6a7582fdd776f386d4826345
magicant <magicant@048f04df-13f5-43d7-8114-9f9ceecaec24>
2009-05-01 04:29:56 +0000

    yash: history: update_history: glibc のために fflush を追加
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/trunk@1048 048f04df-13f5-43d7-8114-9f9ceecaec24

--- history.c
+++ history.c
@@ -657,13 +657,18 @@ void update_history(bool refresh)
     if (!histfile)
 	return;
 
-    clearerr(histfile);
+#ifdef __GNU_LIBRARY__
+    fflush(histfile);
+#endif
     posfail = fgetpos(histfile, &pos);
     rev = read_signature(histfile);
     if (rev < 0) {
 	return;
     } else if (!posfail && rev == histfilerev) {
 	/* The revision has not been changed. Just read new entries. */
+#ifdef __GNU_LIBRARY__
+	fflush(histfile);
+#endif
 	fsetpos(histfile, &pos);
     } else {
 	/* The revision has been changed. Re-read everything. */
