6e8898bff9892a7eacb87f10b743756113e8445a
magicant <magicant@048f04df-13f5-43d7-8114-9f9ceecaec24>
2010-01-04 06:21:24 +0000

    yash: 著作権表示の年を 2010 に
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/trunk@1738 048f04df-13f5-43d7-8114-9f9ceecaec24

--- common.h
+++ common.h
@@ -1,6 +1,6 @@
 /* Yash: yet another shell */
 /* common.h: defines symbols common to all sources. */
-/* (C) 2007-2009 magicant */
+/* (C) 2007-2010 magicant */
 
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
