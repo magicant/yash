b1fd607f3395b4672929d1c49f7a4c12803d96d2
magicant <magicant@048f04df-13f5-43d7-8114-9f9ceecaec24>
2010-09-11 10:23:03 +0000

    yash-complete: display: print_candidate_rawvalue 	CT_FILE 候補の値が / で終わるとき rawvalue が空文字列になってしまっていた
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/branches/complete@2232 048f04df-13f5-43d7-8114-9f9ceecaec24

--- lineedit/display.c
+++ lineedit/display.c
@@ -918,9 +918,12 @@ void print_candidate_rawvalue(const le_candidate_T *cand)
 
     if (cand->type == CT_FILE) {
 	/* skip directory components for a file candidate */
-	const wchar_t *ss = wcsrchr(s, L'/');
-	if (ss != NULL)
-	    s = ss + 1;
+	for (;;) {
+	    const wchar_t *ss = wcschr(s, L'/');
+	    if (ss == NULL || *++ss == L'\0')
+		break;
+	    s = ss;
+	}
     } else if (cand->type == CT_OPTION) {
 	/* prepend a hyphen if none */
 	if (cand->value[0] != L'-')
