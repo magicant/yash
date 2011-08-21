729e91ae4ad4d29f6bb56b9cb4bc4fab59957fa7
magicant <magicant@048f04df-13f5-43d7-8114-9f9ceecaec24>
2011-08-21 09:38:34 +0000

    yash: trietest, trietestw: REFACTOR: indentation
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/trunk@2653 048f04df-13f5-43d7-8114-9f9ceecaec24

--- lineedit/trietest.c
+++ lineedit/trietest.c
@@ -12,110 +12,112 @@
 
 void alloc_failed(void)
 {
-	abort();
+    abort();
 }
 
 void print(const trie_T *t, const char *key)
 {
-	trieget_T tg = trie_get(t, key, strlen(key));
+    trieget_T tg = trie_get(t, key, strlen(key));
 
-	printf("%-10s: ", key);
-	switch (tg.type) {
-		case TG_NOMATCH:
-			printf("nomatch %zu\n", tg.matchlength);
-			break;
-		case TG_EXACTMATCH:
-			printf("exact   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
-			break;
-		case TG_PREFIXMATCH:
-			printf("prefix  %zu\n", tg.matchlength);
-			break;
-		case TG_AMBIGUOUS:
-			printf("ambig   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
-			break;
-		default:
-			printf("ERROR: type=%d\n", (int) tg.type);
-			assert(false);
-	}
+    printf("%-10s: ", key);
+    switch (tg.type) {
+	case TG_NOMATCH:
+	    printf("nomatch %zu\n", tg.matchlength);
+	    break;
+	case TG_EXACTMATCH:
+	    printf("exact   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
+	    break;
+	case TG_PREFIXMATCH:
+	    printf("prefix  %zu\n", tg.matchlength);
+	    break;
+	case TG_AMBIGUOUS:
+	    printf("ambig   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
+	    break;
+	default:
+	    printf("ERROR: type=%d\n", (int) tg.type);
+	    assert(false);
+    }
 }
 
 void print_null(const trie_T *t)
 {
-	trieget_T tg = trie_get(t, "", 1);
+    trieget_T tg = trie_get(t, "", 1);
 
-	printf("<null>    : ");
-	switch (tg.type) {
-		case TG_NOMATCH:
-			printf("nomatch %zu\n", tg.matchlength);
-			break;
-		case TG_EXACTMATCH:
-			printf("exact   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
-			break;
-		case TG_PREFIXMATCH:
-			printf("prefix  %zu\n", tg.matchlength);
-			break;
-		case TG_AMBIGUOUS:
-			printf("ambig   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
-			break;
-		default:
-			printf("ERROR: type=%d\n", (int) tg.type);
-			assert(false);
-	}
+    printf("<null>    : ");
+    switch (tg.type) {
+	case TG_NOMATCH:
+	    printf("nomatch %zu\n", tg.matchlength);
+	    break;
+	case TG_EXACTMATCH:
+	    printf("exact   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
+	    break;
+	case TG_PREFIXMATCH:
+	    printf("prefix  %zu\n", tg.matchlength);
+	    break;
+	case TG_AMBIGUOUS:
+	    printf("ambig   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
+	    break;
+	default:
+	    printf("ERROR: type=%d\n", (int) tg.type);
+	    assert(false);
+    }
 }
 
 int main(int argc, char **argv)
 {
-	(void) argc, (void) argv;
+    (void) argc, (void) argv;
 
-	trie_T *t = trie_create();
+    trie_T *t = trie_create();
 
-	print_null(t);
-	print(t, "");
+    print_null(t);
+    print(t, "");
 
 #define make_trievalue(s) ((trievalue_T) { .keyseq = (s) })
 
-	t = trie_set_null(t, make_trievalue(L"Null"));
-	t = trie_set(t, "abc", make_trievalue(L"ABC"));
-	t = trie_set(t, "abca", make_trievalue(L"ABCA"));
-	t = trie_set(t, "abcb", make_trievalue(L"ABCB"));
-	t = trie_set(t, "abcc", make_trievalue(L"ABCC"));
-	t = trie_set(t, "abcd", make_trievalue(L""));
-	t = trie_set(t, "abcd", make_trievalue(L"ABCD"));
-	t = trie_set(t, "abcde", make_trievalue(L"ABCDE"));
-	t = trie_set(t, "abce", make_trievalue(L"ABCE"));
-	t = trie_set(t, "abcf", make_trievalue(L"ABCF"));
-	t = trie_set(t, "abcg", make_trievalue(L"ABCG"));
-	t = trie_set(t, "abch", make_trievalue(L"ABCH"));
-	t = trie_set(t, "b", make_trievalue(L"B"));
+    t = trie_set_null(t, make_trievalue(L"Null"));
+    t = trie_set(t, "abc", make_trievalue(L"ABC"));
+    t = trie_set(t, "abca", make_trievalue(L"ABCA"));
+    t = trie_set(t, "abcb", make_trievalue(L"ABCB"));
+    t = trie_set(t, "abcc", make_trievalue(L"ABCC"));
+    t = trie_set(t, "abcd", make_trievalue(L""));
+    t = trie_set(t, "abcd", make_trievalue(L"ABCD"));
+    t = trie_set(t, "abcde", make_trievalue(L"ABCDE"));
+    t = trie_set(t, "abce", make_trievalue(L"ABCE"));
+    t = trie_set(t, "abcf", make_trievalue(L"ABCF"));
+    t = trie_set(t, "abcg", make_trievalue(L"ABCG"));
+    t = trie_set(t, "abch", make_trievalue(L"ABCH"));
+    t = trie_set(t, "b", make_trievalue(L"B"));
 
-	print_null(t);
-	print(t, "");
-	print(t, "ab");
-	print(t, "abc");
-	print(t, "abcd");
-	print(t, "abcde");
-	print(t, "abcdex");
-	print(t, "abcdx");
-	print(t, "abcx");
-	print(t, "abx");
-	print(t, "ax");
-	print(t, "b");
-	print(t, "x");
+    print_null(t);
+    print(t, "");
+    print(t, "ab");
+    print(t, "abc");
+    print(t, "abcd");
+    print(t, "abcde");
+    print(t, "abcdex");
+    print(t, "abcdx");
+    print(t, "abcx");
+    print(t, "abx");
+    print(t, "ax");
+    print(t, "b");
+    print(t, "x");
 
-	t = trie_remove(t, "b");
-	t = trie_remove(t, "c");
-	t = trie_set(t, "a", make_trievalue(L"A"));
+    t = trie_remove(t, "b");
+    t = trie_remove(t, "c");
+    t = trie_set(t, "a", make_trievalue(L"A"));
 
-	print(t, "a");
-	print(t, "ax");
-	print(t, "ab");
-	print(t, "abx");
-	print(t, "abc");
-	print(t, "abcx");
-	print(t, "b");
-	print(t, "x");
+    print(t, "a");
+    print(t, "ax");
+    print(t, "ab");
+    print(t, "abx");
+    print(t, "abc");
+    print(t, "abcx");
+    print(t, "b");
+    print(t, "x");
 
-	trie_destroy(t);
+    trie_destroy(t);
 
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
+
+/* vim: set ts=8 sts=4 sw=4 noet tw=80: */
--- lineedit/trietestw.c
+++ lineedit/trietestw.c
@@ -11,83 +11,85 @@
 
 void alloc_failed(void)
 {
-	abort();
+    abort();
 }
 
 void print(const trie_T *t, const wchar_t *key)
 {
-	trieget_T tg = trie_getw(t, key);
+    trieget_T tg = trie_getw(t, key);
 
-	printf("%-10ls: ", key);
-	switch (tg.type) {
-		case TG_NOMATCH:
-			printf("nomatch %zu\n", tg.matchlength);
-			break;
-		case TG_EXACTMATCH:
-			printf("exact   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
-			break;
-		case TG_PREFIXMATCH:
-			printf("prefix  %zu\n", tg.matchlength);
-			break;
-		case TG_AMBIGUOUS:
-			printf("ambig   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
-			break;
-		default:
-			printf("ERROR: type=%d\n", (int) tg.type);
-			assert(false);
-	}
+    printf("%-10ls: ", key);
+    switch (tg.type) {
+	case TG_NOMATCH:
+	    printf("nomatch %zu\n", tg.matchlength);
+	    break;
+	case TG_EXACTMATCH:
+	    printf("exact   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
+	    break;
+	case TG_PREFIXMATCH:
+	    printf("prefix  %zu\n", tg.matchlength);
+	    break;
+	case TG_AMBIGUOUS:
+	    printf("ambig   %zu (%ls)\n", tg.matchlength, tg.value.keyseq);
+	    break;
+	default:
+	    printf("ERROR: type=%d\n", (int) tg.type);
+	    assert(false);
+    }
 }
 
 int main(int argc, char **argv)
 {
-	(void) argc, (void) argv;
+    (void) argc, (void) argv;
 
-	trie_T *t = trie_create();
+    trie_T *t = trie_create();
 
-	print(t, L"");
+    print(t, L"");
 
 #define make_trievalue(s) ((trievalue_T) { .keyseq = (s) })
 
-	t = trie_setw(t, L"abc", make_trievalue(L"ABC"));
-	t = trie_setw(t, L"abca", make_trievalue(L"ABCA"));
-	t = trie_setw(t, L"abcb", make_trievalue(L"ABCB"));
-	t = trie_setw(t, L"abcc", make_trievalue(L"ABCC"));
-	t = trie_setw(t, L"abcd", make_trievalue(L""));
-	t = trie_setw(t, L"abcd", make_trievalue(L"ABCD"));
-	t = trie_setw(t, L"abcde", make_trievalue(L"ABCDE"));
-	t = trie_setw(t, L"abce", make_trievalue(L"ABCE"));
-	t = trie_setw(t, L"abcf", make_trievalue(L"ABCF"));
-	t = trie_setw(t, L"abcg", make_trievalue(L"ABCG"));
-	t = trie_setw(t, L"abch", make_trievalue(L"ABCH"));
-	t = trie_setw(t, L"b", make_trievalue(L"B"));
+    t = trie_setw(t, L"abc", make_trievalue(L"ABC"));
+    t = trie_setw(t, L"abca", make_trievalue(L"ABCA"));
+    t = trie_setw(t, L"abcb", make_trievalue(L"ABCB"));
+    t = trie_setw(t, L"abcc", make_trievalue(L"ABCC"));
+    t = trie_setw(t, L"abcd", make_trievalue(L""));
+    t = trie_setw(t, L"abcd", make_trievalue(L"ABCD"));
+    t = trie_setw(t, L"abcde", make_trievalue(L"ABCDE"));
+    t = trie_setw(t, L"abce", make_trievalue(L"ABCE"));
+    t = trie_setw(t, L"abcf", make_trievalue(L"ABCF"));
+    t = trie_setw(t, L"abcg", make_trievalue(L"ABCG"));
+    t = trie_setw(t, L"abch", make_trievalue(L"ABCH"));
+    t = trie_setw(t, L"b", make_trievalue(L"B"));
 
-	print(t, L"");
-	print(t, L"ab");
-	print(t, L"abc");
-	print(t, L"abcd");
-	print(t, L"abcde");
-	print(t, L"abcdex");
-	print(t, L"abcdx");
-	print(t, L"abcx");
-	print(t, L"abx");
-	print(t, L"ax");
-	print(t, L"b");
-	print(t, L"x");
+    print(t, L"");
+    print(t, L"ab");
+    print(t, L"abc");
+    print(t, L"abcd");
+    print(t, L"abcde");
+    print(t, L"abcdex");
+    print(t, L"abcdx");
+    print(t, L"abcx");
+    print(t, L"abx");
+    print(t, L"ax");
+    print(t, L"b");
+    print(t, L"x");
 
-	t = trie_removew(t, L"b");
-	t = trie_removew(t, L"c");
-	t = trie_setw(t, L"a", make_trievalue(L"A"));
+    t = trie_removew(t, L"b");
+    t = trie_removew(t, L"c");
+    t = trie_setw(t, L"a", make_trievalue(L"A"));
 
-	print(t, L"a");
-	print(t, L"ax");
-	print(t, L"ab");
-	print(t, L"abx");
-	print(t, L"abc");
-	print(t, L"abcx");
-	print(t, L"b");
-	print(t, L"x");
+    print(t, L"a");
+    print(t, L"ax");
+    print(t, L"ab");
+    print(t, L"abx");
+    print(t, L"abc");
+    print(t, L"abcx");
+    print(t, L"b");
+    print(t, L"x");
 
-	trie_destroy(t);
+    trie_destroy(t);
 
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
+
+/* vim: set ts=8 sts=4 sw=4 noet tw=80: */
