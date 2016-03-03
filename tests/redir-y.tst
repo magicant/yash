magicant/yash

commit 593a02429aa64c1e8329eb534420019e5a790426

Remove quotes after empty field removal
This is a refactoring commit that adds a quote removal step in the word
expansion process. As required by POSIX, quote removal is performed
after field splitting.

Unlike POSIX, quote removal is performed after empty field removal
because presence of quotation characters affects empty field removal.

Still unlike POSIX, quote removal is performed before pathname expansion
(globbing) because the current implementation of pathname expansion does
not support single- and double-quotation characters. This does not break
POSIX conformance since quoted characters are correctly escaped before
they are passed to the pathname expansion step.

Currently, the quote removal step added in this commit is a no-op
because quotes are already removed in the four expansions (in the
expand_word_inner function). The upcoming commit will modify this
behavior so that the new step is actually effective.

git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/branches/emptyfieldremoval@3588 048f04df-13f5-43d7-8114-9f9ceecaec24

expand.c
@@ -43,6 +43,12 @@
 #include "yash.h"
 
 
+/* characters that have special meanings in brace expansion, quote removal, and
+ * globbing. When an unquoted expansion includes these characters, they are
+ * backslashed to protect from unexpected side effects in succeeding expansion
+ * steps. */
+#define CHARS_ESCAPED L"\\\"\'{,}"
+
 static bool expand_and_split_words(
 	const wordunit_T *restrict w, plist_T *restrict list)
     __attribute__((nonnull(2)));
@@ -130,6 +136,10 @@ static inline void add_sq(
     __attribute__((nonnull));
 static wchar_t *escaped_wcspbrk(const wchar_t *s, const wchar_t *accept)
     __attribute__((nonnull));
+static wchar_t *escaped_remove(const wchar_t *s, const wchar_t *reject)
+    __attribute__((nonnull,malloc,warn_unused_result));
+static inline wchar_t *escaped_remove_free(wchar_t *s, const wchar_t *reject)
+    __attribute__((nonnull,malloc,warn_unused_result));
 
 static void glob_all(void **restrict patterns, plist_T *restrict list)
     __attribute__((nonnull));
@@ -255,6 +265,10 @@ bool expand_and_split_words(
 	    pl_remove(list, oldlength, 1);
 	}
     }
+
+    /* quote removal */
+    for (size_t i = oldlength; i < list->length; i++)
+	list->contents[i] = escaped_remove_free(list->contents[i], L"\"\'");
 
     return true;
 }
@@ -423,6 +437,7 @@ bool expand_word(
 	const wordunit_T *restrict w, tildetype_T tilde, bool quoted,
 	plist_T *restrict valuelist)
 {
+    size_t oldlength = valuelist->length;
     struct expand_word_T expand;
 
     expand.valuelist = valuelist;
@@ -438,6 +453,11 @@ bool expand_word(
 	pl_add(expand.valuelist, wb_towcs(&expand.valuebuf));
     else
 	wb_destroy(&expand.valuebuf);
+
+    /* quote removal */
+    for (size_t i = oldlength; i < valuelist->length; i++)
+	valuelist->contents[i] =
+	    escaped_remove_free(valuelist->contents[i], L"\"\'");
 
     return ok;
 }
@@ -1734,6 +1754,33 @@ wchar_t *escaped_wcspbrk(const wchar_t *s, const wchar_t *accept)
 	    return (wchar_t *) s;
     }
     return NULL;
+}
+
+/* Removes characters in `reject' from `s'.
+ * Backslash escapes in `s' are recognized. Escapes and escaped characters are
+ * kept in the result.
+ * The result is a newly malloced string. */
+wchar_t *escaped_remove(const wchar_t *s, const wchar_t *reject)
+{
+    xwcsbuf_T result;
+    wb_init(&result);
+    for (;;) {
+	const wchar_t *rejectchar = escaped_wcspbrk(s, reject);
+	if (rejectchar == NULL)
+	    break;
+	wb_ncat_force(&result, s, rejectchar - s);
+	s = rejectchar + 1;
+    }
+    wb_cat(&result, s);
+    return wb_towcs(&result);
+}
+
+/* Like `escaped_remove', but frees `s' before returning the result. */
+wchar_t *escaped_remove_free(wchar_t *s, const wchar_t *reject)
+{
+    wchar_t *result = escaped_remove(s, reject);
+    free(s);
+    return result;
 }
 
 

expand.h
@@ -1,6 +1,6 @@
 /* Yash: yet another shell */
 /* expand.h: word expansion */
-/* (C) 2007-2010 magicant */
+/* (C) 2007-2016 magicant */
 
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,9 +23,6 @@
 
 
 #define DEFAULT_IFS L" \t\n"
-
-/* characters that have special meanings in brace expansion and glob. */
-#define CHARS_ESCAPED L"\\{,}"
 
 /* characters that can be escaped with a backslash inside double-quotes. */
 #define CHARS_ESCAPABLE L"$`\"\\"

commit fd453ca1729ad6b601495b384122e0c7e81e9371

Remove quotes in expand_param_simple
This commit fixes a bug introduced in r3588 where quote removal was
missing in the expand_param_simple function.

git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/yash/yash/trunk@3596 048f04df-13f5-43d7-8114-9f9ceecaec24

expand.c
@@ -987,8 +987,10 @@ wchar_t *expand_param_simple(const paramexp_T *p)
     else
 	pl_add(&valuelist, wb_towcs(&expand.valuebuf));
 
-    for (size_t i = 0; i < valuelist.length; i++)
-	valuelist.contents[i] = unescapefree(valuelist.contents[i]);
+    for (size_t i = 0; i < valuelist.length; i++) {
+	wchar_t *v = escaped_remove_free(valuelist.contents[i], L"\"\'");
+	valuelist.contents[i] = unescapefree(v);
+    }
 
     void **results = pl_toary(expand.valuelist);
     wchar_t *result = ok ? joinwcsarray(results, L" ") : NULL;

tests/redir-y.tst
@@ -71,6 +71,25 @@ __IN__
 foo
 __OUT__
 
+test_oE -e 0 'complex expansion in here-document' -s 1 '"  "' 3
+IFS=-
+printf '[%s]' ${1+"$@"}; echo
+printf '[%s]' "${1+"$@"}"; echo
+cat <<END
+[$*]
+[$@]
+[${1+"$*"}]
+[${1+"$@"}]
+END
+__IN__
+[1]["  "][3]
+[1]["  "][3]
+[1-"  "-3]
+[1-"  "-3]
+[1-"  "-3]
+[1 "  " 3]
+__OUT__
+
 test_oE -e 0 'duplicating input to the same file descriptor'
 echo foo | cat <&0
 __IN__