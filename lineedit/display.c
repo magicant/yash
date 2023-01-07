424594d636f70f247ef1552bd4fbbacf4f5019e6
leath-dub <77889270+leath-dub@users.noreply.github.com>
2023-01-07 16:55:12 +0000

    fixed right prompt right justification being off by a single cell

--- lineedit/display.c
+++ lineedit/display.c
@@ -786,10 +786,10 @@ void update_right_prompt(void)
 	return;
     if (rprompt.width == 0)
 	return;
-    if (lebuf.maxcolumn - rprompt.width - 2 < 0)
+    if (lebuf.maxcolumn - rprompt.width - 1 < 0)
 	return;
     int c = cursor_positions[le_main_buffer.length] % lebuf.maxcolumn;
-    bool has_enough_room = (c <= lebuf.maxcolumn - rprompt.width - 2);
+    bool has_enough_room = (c <= lebuf.maxcolumn - rprompt.width - 1);
     if (!has_enough_room && !shopt_le_alwaysrp)
 	return;
 
@@ -798,9 +798,9 @@ void update_right_prompt(void)
 	lebuf_print_nel();
 	check_cand_overwritten();
     }
-    lebuf_print_cuf(lebuf.maxcolumn - rprompt.width - lebuf.pos.column - 1);
+    lebuf_print_cuf(lebuf.maxcolumn - rprompt.width - lebuf.pos.column);
     sb_ncat_force(&lebuf.buf, rprompt.value, rprompt.length);
-    lebuf.pos.column += rprompt.width;
+    lebuf.pos.column += rprompt.width - 1;
     last_edit_line = rprompt_line = lebuf.pos.line;
     styler_active = false;
 }
